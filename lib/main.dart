6601ccc0ad742b53288a9edaad279978a475ea25
raza4399 <52241717+raza4399@users.noreply.github.com>
2020-10-02 09:30:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
+      initialRoute: "crud_sample",
+      routes: {
+       "crud_sample": (context) => CrudSample(),
+//        "signup": (context) => SignupScreen(),
+//        "login": (context) => LoginScreen(),
+//        "home": (context) => Myhome(),
+//        "sup": (context) => Sup(),
+      },
       title: 'Flutter Demo',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      navigatorObservers: <NavigatorObserver>[observer],
-      // home: new WallScreen(analytics: analytics, observer: observer),
-      home: new CrudSample(),
     );
   }
 }
